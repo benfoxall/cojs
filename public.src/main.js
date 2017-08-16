60d35ffa418e64249fd8ff2d5ffc51f8de6aa23e
Ben Foxall <benfoxall@gmail.com>
2017-08-16 22:24:19 +0100

    Switch to new UI styles and renderer

--- public.src/main.js
+++ public.src/main.js
@@ -1,4 +1,12 @@
 import evaluate from './evaluate'
 import parse from './parse'
+import render from './render'
 
-export {evaluate, parse}
+
+const s = document.currentScript
+if(s && s.hasAttribute('data-render'))
+  render(document.body)
+
+
+
+export {evaluate, parse, render}

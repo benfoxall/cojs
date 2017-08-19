e9534e3e24a1c3d57ee32f24b0e9f16699e9bbde
Ben Foxall <benfoxall@gmail.com>
2017-08-19 12:32:59 +0100

    Backend process counter

--- handler.js
+++ handler.js
@@ -1,16 +1,48 @@
 'use strict';
 
+const AWS = require('aws-sdk')
+
+const options = {}
+if (process.env.IS_OFFLINE) {
+  Object.assign(options, {
+    region: 'localhost',
+    endpoint: 'http://localhost:8000',
+  })
+}
+
+const db = new AWS.DynamoDB.DocumentClient(options);
+
 module.exports.helloWorld = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      session: '00-process-counter',
+      ref: 0
     },
-    body: JSON.stringify({
-      message: 'hello from the cojs backend'
-      // input: event,
-    }),
+    ExpressionAttributeNames: {
+      '#count': 'count',
+    },
+    ExpressionAttributeValues: {
+      ':number': 1,
+    },
+    UpdateExpression: 'ADD #count :number',
+    ReturnValues: 'ALL_NEW',
   };
 
-  callback(null, response);
+  db.update(params, (error, updated) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(new Error('Couldn\'t create item.'));
+      return;
+    }
+
+    const response = {
+      statusCode: 200,
+      body: updated.Attributes.count,
+    };
+    callback(null, response);
+  });
+
 };

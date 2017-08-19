1dfbedb0c13791c85f07890273fbae468ba9d70c
Ben Foxall <benfoxall@gmail.com>
2017-08-19 19:14:13 +0100

    Web Tokens

--- backend/src/session.js
+++ backend/src/session.js
@@ -1,4 +1,5 @@
-const {db, generateProcessId} = require('./db');
+const {db, generateProcessId} = require('./db')
+const jwt = require('jsonwebtoken')
 
 
 const processId = generateProcessId()
@@ -8,19 +9,28 @@ processId.then(id => console.log(`ProcessId: ${id}`))
 let count = 0
 
 module.exports.create = (event, context, callback) => {
-
-
   processId
     .then(id => {
 
+      // create a session id
+      const session = [id, count++]
+
+      // generate a token for 1 day
+      const token = jwt.sign(
+        { session },
+        process.env.JWT_SECRET,
+        {expiresIn: '1d'}
+      )
+
+
       const response = {
         statusCode: 200,
-        body: JSON.stringify([
-          id, count++
-        ])
+        body: JSON.stringify({
+          session: session,
+          token: token
+        })
       };
       callback(null, response);
 
     })
-
 }

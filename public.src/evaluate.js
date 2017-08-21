2ce73916f4176ac678330dc91e120afa11b82aaa
Ben Foxall <benfoxall@gmail.com>
2017-08-22 00:28:15 +0100

    Use global loop protect

--- public.src/evaluate.js
+++ public.src/evaluate.js
@@ -1,18 +1,17 @@
 import loopProtect from 'loop-protect'
 
-loopProtect.method = '__protect';
+window.runnerWindow = loopProtect
 
 const evaluate = (code, state, gives, takes) => {
-
   const processed = loopProtect.rewriteLoops(code)
 
   const intrumented = `${processed}; return {${gives.join(', ')}}`
 
-  const args = takes.concat('__protect').concat(intrumented)
+  const args = takes.concat(intrumented)
 
   const fn = Function.apply(null, args)
 
-  return fn.apply(null, takes.map(v => state[v]).concat(loopProtect.protect))
+  return fn.apply(null, takes.map(v => state[v]))
 
 }
 

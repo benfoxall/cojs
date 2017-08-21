9412f319e8f919d2825d1be6a5293977d5803bd7
Ben Foxall <benfoxall@gmail.com>
2017-08-22 00:57:45 +0100

    Remove State (I was drunk)

--- public.src/main.js
+++ public.src/main.js
@@ -2,7 +2,6 @@ import evaluate from './evaluate'
 import parse from './parse'
 import render from './render'
 import Session from './Session'
-import State from './State'
 import {BasicController} from './controllers'
 import iframeEvaluator from './iframeEvaluator'
 
